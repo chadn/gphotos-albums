b563e68dd3d687409e8b488b8682a378922be02a
Chad Norwood <chad@chadnorwood.com>
2024-11-04 22:15:33 -0800

    adding login-logout

---
+++ src/components/login-logout.tsx
@@ -0,0 +1,28 @@
+import { auth } from "@/auth"
+import SignIn from "@/components/sign-in";
+import Image from "next/image";
+
+export default async function LoginLogout() {
+  const session = await auth();
+  if (!session?.user) {
+    return (
+      <div>
+        <SignIn />
+      </div>
+    )    
+  } else {
+    const imgSrc = session?.user?.image || ''
+    const imgAlt = session?.user?.name || 'no img'
+    return (
+      <div>
+        <h2>{session?.user?.name}</h2>
+        <Image
+          src={imgSrc}
+          alt={imgAlt}
+          width={72}
+          height={72}
+          className="rounded-full" />
+      </div>
+    )
+  }
+} 
