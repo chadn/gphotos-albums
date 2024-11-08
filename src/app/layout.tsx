a0a08f185cc796e1f0e1b7f7b76dd45f1b2484ab
Chad Norwood <chad@chadnorwood.com>
2024-11-08 14:50:29 -0800

    add favico, cleanup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,8 +27,17 @@ export default async function RootLayout({
 }>) {
   const session = await auth();
 
+  // favicon generated from https://realfavicongenerator.net/
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <meta name="apple-mobile-web-app-title" content="GetMovin" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
