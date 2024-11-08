5fd979a79a85475683ddc46012c6b945128fcdab
Chad Norwood <chad@chadnorwood.com>
2024-11-07 17:06:31 -0800

    Google auth almost works now, adding src/middleware back to see what happens, and also added logging to next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  logging: {
+    fetches: {
+      fullUrl: true, // Log the full URL for fetches
+      hmrRefreshes: true // Log fetches from HMR cache
+    }
+  },
   images: {
     remotePatterns: [
       {
