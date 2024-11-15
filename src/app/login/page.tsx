b8ef23174749596321bdba8773cd2efd9d9cb15c
Chad Norwood <chad@chadnorwood.com>
2024-11-15 09:47:55 -0800

    refactor: moving Login Button from login/page.tsx to components/LoginButton.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,24 +1,13 @@
 'use client';
-import { signIn } from 'next-auth/react';
 import React from 'react';
 
-import Button from '@/components/buttons/Button';
+import LoginButton from '@/components/LoginButton';
 
 function page() {
   return (
     <div>
       <div className='flex flex-wrap gap-2 items-center justify-center'>
-        <Button
-          variant='primary'
-          type='submit'
-          onClick={() =>
-            signIn('google', {
-              callbackUrl: '/my-account',
-            })
-          }
-        >
-          Login with Google
-        </Button>
+        <LoginButton />
         to retreive information on your Google Photos Albums
       </div>
     </div>
