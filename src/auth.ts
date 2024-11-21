7d422bec54bfea21ad49675826b62f97df97d64f
Chad Norwood <chad@chadnorwood.com>
2024-11-21 11:39:40 -0800

    fix: using module-augmentation to extend session.user to include given_name

--- src/auth.ts
+++ src/auth.ts
@@ -1,21 +1,39 @@
 // @/auth.ts
 
-import NextAuth from 'next-auth';
-import type { NextAuthConfig } from 'next-auth';
+import NextAuth, { type DefaultSession, type NextAuthConfig } from 'next-auth';
 
 import { authConfig } from '@/lib/auth.config';
+
+//import type { AdapterUser } from 'next-auth/core/adapters';
+
+// https://authjs.dev/getting-started/typescript#module-augmentation
+declare module 'next-auth' {
+  /**
+   * Returned by `auth`, `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+   */
+  interface Session {
+    user: {
+      /** The user's given name. */
+      given_name: string;
+      /** The user's access_token for subsequent calls to Google API */
+      access_token: string;
+      /**
+       * By default, TypeScript merges new interface properties and overwrites existing ones.
+       * In this case, the default session user properties will be overwritten,
+       * with the new ones defined above. To keep the default session user properties,
+       * you need to add them back into the newly declared interface.
+       */
+    } & DefaultSession['user'];
+  }
+}
 
 export const config = {
   ...authConfig,
   debug: !!process.env.AUTH_DEBUG,
   trustHost: true,
   callbacks: {
-    async jwt({ token, account, profile }) {
-      // the following is also logged if AUTH_DEBUG is true
-      // so commenting out.
-
-      /*
     async jwt({ token, user, account, profile }) {
+      // the following is also logged if AUTH_DEBUG is true
       console.log(
         'auth.ts jwt cb',
         JSON.stringify({
@@ -25,9 +43,10 @@ export const config = {
           profile: profile,
         })
       );
-      */
+
       // add what we need to use to token, token info stored in cookies.
       // only assign access_token if not already assigned - this cb gets called lots.
+      // https://authjs.dev/guides/extending-the-session
       token.given_name ??= profile?.given_name ? profile.given_name : null;
       token.access_token ??= account?.access_token
         ? account.access_token
@@ -35,7 +54,7 @@ export const config = {
       console.log('auth.ts jwt returning: token', JSON.stringify(token));
       return token;
     },
-    async session({ session, token }) {
+    session({ session, token, user }) {
       if (token) {
         // set the token data to session
         //
@@ -49,10 +68,19 @@ export const config = {
         'auth.ts session',
         JSON.stringify({
           token: token,
+          user: user,
           session: session,
         })
       );
-      return session;
+      // `session.user.given_name` is now a valid property, and will be type-checked
+      // in places like `useSession().data.user` or `auth().user`
+      return {
+        ...session,
+        user: {
+          ...session.user,
+          //given_name: user.given_name,
+        },
+      };
     },
   },
 } satisfies NextAuthConfig;
